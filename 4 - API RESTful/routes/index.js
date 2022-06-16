93dc956a59a84ff7ff92f2a35cabbf061b57a45c
carpcocolizo <germanvalente@yahoo.com>
2022-06-16 20:29:37 -0300

    actualizacion try/catch

--- 4 - API RESTful/routes/index.js
+++ 4 - API RESTful/routes/index.js
@@ -3,56 +3,83 @@ const router = Router()
 const productos = []
 
 router.get('/productos', (req, res) => {
-    res.json(productos)
+    try {
+        res.json(productos)
+    } catch(error) {
+        console.log("Hubo un error:" + error)
+        res.sendStatus(500)
+    }
 })
 
 router.get('/productos/:id', (req, res) => {
-    const id = Number(req.params.id)
-    if (productos[id - 1] == undefined ) {
-        res.json("{ error: producto no encontrado }")
-        return
+    try {
+      const id = Number(req.params.id)
+      if (productos[id - 1] == undefined ) {
+          res.json("{ error: producto no encontrado }")
+          return
+      }
+      let producto = productos[req.params.id - 1]
+      res.json(producto)
+    } catch(error) {
+        console.log("Hubo un error:" + error)
+        res.sendStatus(500)
     }
-    let producto = productos[req.params.id - 1]
-    res.json(producto)
 })
 
 router.post('/productos', (req, res) => {
-    const { title, price, thumbnail } = req.body
-    if(isNaN(Number(price))) {
-        const error = new Error('El precio tiene que ser un numero')
-        error.httpStatusCode(400)
-        next(error, req, res)
+    try {
+        const { title, price, thumbnail } = req.body
+        if(isNaN(Number(price))) {
+            const error = new Error('El precio tiene que ser un numero')
+            error.httpStatusCode(400)
+            next(error, req, res)
+        }
+        let id = productos.length + 1
+        res.sendStatus(201)
+        productos.push({ title, price, thumbnail, id })
+    } catch(error) {
+        console.log("Hubo un error:" + error)
+        res.sendStatus(500)
     }
-    let id = productos.length + 1
-    productos.push({ title, price, thumbnail, id })
-
-    res.sendStatus(201)
 })
+        
+        
 
 router.put('/productos/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const { title, price, thumbnail } = req.body
-    if(isNaN(Number(price))) {
-        const error = new Error('El precio tiene que ser un numero')
-        error.httpStatusCode(400)
-        next(error, req, res)
+    try {
+        const id = Number(req.params.id)
+        const { title, price, thumbnail } = req.body
+        if(isNaN(Number(price))) {
+            const error = new Error('El precio tiene que ser un numero')
+            error.httpStatusCode(400)
+            next(error, req, res)
+        }
+        productos[id - 1] = ({ title, price, thumbnail, id })
+        res.sendStatus(202)
+    } catch(error) {
+        console.log("Hubo un error:" + error)
+        res.sendStatus(500)
     }
-    productos[id - 1] = ({ title, price, thumbnail, id })
-
-    res.sendStatus(202)
 })
+        
+        
 
 router.delete('/productos/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const { title, price, thumbnail } = req.body
-    if(isNaN(Number(price))) {
-        const error = new Error('El precio tiene que ser un numero')
-        error.httpStatusCode(400)
-        next(error, req, res)
+    try {
+        const id = Number(req.params.id)
+        const { title, price, thumbnail } = req.body
+        if(isNaN(Number(price))) {
+            const error = new Error('El precio tiene que ser un numero')
+            error.httpStatusCode(400)
+            next(error, req, res)
+        }
+        productos[id - 1] = null
+        res.sendStatus(202)
+    } catch(error) {
+        console.log("Hubo un error:" + error)
+        res.sendStatus(500)
     }
-    productos[id - 1] = null
-
-    res.sendStatus(202)
 })
-
+        
 module.exports = router
+        
